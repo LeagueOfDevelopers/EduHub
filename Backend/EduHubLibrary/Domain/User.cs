c649209c1ca1785c9bc87df5211c8b3bd5e6467a
Alena Ionova <ionova0010@gmail.com>
2017-12-15 18:02:19 +0300

    User's aggregate arrived

--- Backend/EduHubLibrary/Domain/User.cs
+++ Backend/EduHubLibrary/Domain/User.cs
@@ -9,134 +9,34 @@ namespace EduHubLibrary.Domain
     {
         public string Name { get; private set; }
         public string Email { get; private set; }
+        public bool IsTeacher { get; private set; }
+        public bool IsActive { get; private set; }
         public Guid Id { get; private set; }
 
         public User(string name, string email, bool isTeacher)
         {
             Name = name;
             Email = email;
-            _isTeacher = isTeacher;
-            _isActive = true;
+            IsTeacher = isTeacher;
+            IsActive = true;
             Id = Guid.NewGuid();
         }
 
-        public void EditProfile(string newName, bool isTeacher)
+        public void EditProfile(string newName, string email, bool isTeacher)
         {
             Name = newName;
-            _isTeacher = isTeacher;
+            Email = email;
+            IsTeacher = isTeacher;
         }
 
         public void RestoreProfile()
         {
-            _isActive = true;
+            IsActive = true;
         }
 
         public void DeleteProfile()
         {
-            _isActive = false;
+            IsActive = false;
         }
-
-        public void InviteToGroup(Group group, RoleInChat role)
-        {
-            if (_isActive)
-            {
-                if (_memberShips.Any(s => s.Group.Equals(group)))
-                {
-                    throw new Exception("Пользователь уже приглашен или состоит в этой группе");
-                }
-                else
-                {
-                    _memberShips.Add(new MemberShip(group, role, StatusInChat.Invited));
-                }
-            }
-            else throw new Exception("Пользователь неактивен");
-        }
-
-        public void AcceptInvitation(Group group)
-        {
-            if (_memberShips.Any(s => s.Group.Equals(group)))
-            {
-                throw new Exception("Пользователь не приглашен в эту группу");
-            }
-            else
-            {
-                MemberShip Membership = _memberShips.First(s => s.Group.Equals(group));
-                Membership.Status = StatusInChat.Waiting;
-            }
-        }
-
-        public void RejectInvitation(Group group)
-        {
-            if (_memberShips.Any(s => s.Group.Equals(group)))
-            {
-                throw new Exception("Пользователь не приглашен в эту группу");
-            }
-            else
-            {
-                MemberShip Membership = _memberShips.First(s => s.Group.Equals(group));
-                _memberShips.Remove(Membership);
-            }
-        }
-
-        public void DeleteFromGroup(Group group)
-        {
-            if (_memberShips.Any(s => s.Group.Equals(group)))
-            {
-                throw new Exception("Пользователь не состоит в этой группе");
-            }
-            else
-            {
-                MemberShip Membership = _memberShips.First(s => s.Group.Equals(group));
-
-                if (Membership.Role == RoleInChat.Teacher)
-                {
-                    throw new Exception("Преподаватель не может покинуть группу, не закрыв курс");
-                }
-                else
-                {
-                    _memberShips.Remove(Membership);
-                }
-            }
-        }
-
-        bool _isTeacher;
-        bool _isActive;
-        List<MemberShip> _memberShips;
     }
-
-    class MemberShip
-    {
-        public Group Group
-        {
-            get { return _group; }
-            private set { _group = value; }
-        }
-        Group _group;
-
-        public RoleInChat Role { get; private set; }
-        public StatusInChat Status { get; set; }
-
-        public MemberShip(Group group, RoleInChat role, StatusInChat status)
-        {
-            _group = group;
-            Role = role;
-            Status = status;
-            Restrictions = new List<Punishment>();
-        }
-
-        public void AddPunishment()
-        {
-            Restrictions.Add(new Punishment());
-        }
-
-        List<Punishment> Restrictions;
-    }
-
-    struct Punishment
-    {
-        //Параметры санкций
-    }
-
-    public enum RoleInChat { Creator, Teacher, Student }
-    public enum StatusInChat { Invited, Waiting, Educating }
 }
---
+++ Backend/UnitTests/UserTests.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EduHubLibrary.Domain;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UserTests
+    {
+        [TestMethod]
+        public void CreationOfUserReturnTrue()
+        {
+            string NameOfUser = "Ivan";
+            string Email = "sokolov@gmail.com";
+            bool IsTeacher = false;
+
+            User UserTest = new User(NameOfUser, Email, IsTeacher);
+            string ExpectedName = NameOfUser;
+            string ExpectedEmail = Email;
+            bool ExpectedIsTeacher = IsTeacher;
+
+            string ActualName = UserTest.Name;
+            string ActualEmail = UserTest.Email;
+            bool ActualIsTeacher = UserTest.IsTeacher;
+            Assert.AreEqual(ExpectedName, ActualName);
+            Assert.AreEqual(ExpectedEmail, ActualEmail);
+            Assert.AreEqual(ExpectedIsTeacher, ActualIsTeacher);
+        }
+
+        [TestMethod]
+        public void EditProfileReturnTrue()
+        {
+            string NewName = "Nikolai";
+            string NewEmail = "petrov@gmail.com";
+            bool IsTeacher = false;
+
+            User UserTest = new User("Ivan", "sokolov@gmail.com", true);
+            UserTest.EditProfile(NewName, NewEmail, IsTeacher);
+            string ExpectedName = NewName;
+            string ExpectedEmail = NewEmail;
+            bool ExpectedIsTeacher = IsTeacher;
+
+            string ActualName = UserTest.Name;
+            string ActualEmail = UserTest.Email;
+            bool ActualIsTeacher = UserTest.IsTeacher;
+            Assert.AreEqual(ExpectedName, ActualName);
+            Assert.AreEqual(ExpectedEmail, ActualEmail);
+            Assert.AreEqual(ExpectedIsTeacher, ActualIsTeacher);
+        }
+
+        [TestMethod]
+        public void DeleteProfileReturnTrue()
+        {
+            User UserTest = new User("Ivan", "ivanov@gmail.com", false);
+
+            UserTest.DeleteProfile();
+            bool Expected = false;
+
+            bool Actual = UserTest.IsActive;
+            Assert.AreEqual(Expected, Actual);
+        }
+
+        [TestMethod]
+        public void RestoreProfileReturnTrue()
+        {
+            User UserTest = new User("Ivan", "ivanov@gmail.com", false);
+
+            UserTest.DeleteProfile();
+            UserTest.RestoreProfile();
+            bool Expected = true;
+
+            bool Actual = UserTest.IsActive;
+            Assert.AreEqual(Expected, Actual);
+        }
+    }
+}
