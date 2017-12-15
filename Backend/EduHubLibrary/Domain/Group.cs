6713abe492b78ee4d33506fb6907e49e0946dec2
yaroslav <bus.yaroslav@gmail.com>
2017-12-15 23:13:05 +0300

    netcore project

--- Backend/EduHubLibrary/Domain/Group.cs
+++ Backend/EduHubLibrary/Domain/Group.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Linq;
 using EduHubLibrary.Domain.Exceptions;
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("UnitTests")]
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("EduHubTests")]
 
 namespace EduHubLibrary.Domain
 {
