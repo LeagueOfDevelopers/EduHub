f2b846b86dc6eb763a3a78082d0c9f5f3d94f0d7
yaroslav <bus.yaroslav@gmail.com>
2018-01-07 00:45:42 +0300

    try to fix build

--- Backend/EduHub/Startup.cs
+++ Backend/EduHub/Startup.cs
@@ -83,7 +83,7 @@ namespace EduHub
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            app.UseStaticFiles();
+            //app.UseStaticFiles();
             app.UseSwagger();
 
             app.UseSwaggerUI(current => { 
