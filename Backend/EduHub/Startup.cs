1db6786bbd25280c4215d724031f74b198c2bc40
yaroslav <bus.yaroslav@gmail.com>
2018-01-10 16:34:21 +0300

    changed path

--- Backend/EduHub/Startup.cs
+++ Backend/EduHub/Startup.cs
@@ -63,7 +63,7 @@ namespace EduHub
                 });
                 current.OperationFilter<ExamplesOperationFilter>();
                 current.DescribeAllEnumsAsStrings();
-                var filePath = "Eduhub.xml";
+                var filePath = "../Eduhub.xml";
                 current.IncludeXmlComments(filePath);
             });
             ConfigureSecurity(services);
