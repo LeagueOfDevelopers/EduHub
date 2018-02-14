796dd4cc150186e637be9d6027a8db4911eb78f0
Alena Ionova <ionova0010@gmail.com>
2018-02-14 22:57:38 +0300

    Some thoughts about tags (TagsManager class)

---
+++ Backend/EduHubLibrary/Domain/TagsManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EduHubLibrary.Domain
+{
+    public class TagsManager
+    {
+        public TagsManager()
+        {
+            Tags = new List<string>();
+        }
+
+        public IEnumerable<string> Tags { get; private set; }
+
+        public void AddTag(string newTag)
+        {
+            var newTagsList = new List<string>(Tags);
+            newTagsList.Add(newTag);
+            Tags = newTagsList;
+        }
+
+        public IEnumerable<string> FindTag(string tag)
+        {
+            return Tags.ToList().FindAll(t => t.Contains(tag));
+        }
+    }
+}
