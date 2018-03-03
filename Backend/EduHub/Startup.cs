8ded9eaecc864709a8cfc313b7cf2a55026d68c7
Alena Ionova <ionova0010@gmail.com>
2018-03-03 10:17:24 +0300

    Returned old settings of rabbitmq

--- Backend/EduHub/Startup.cs
+++ Backend/EduHub/Startup.cs
@@ -56,8 +56,11 @@ namespace EduHub
                 Configuration.GetValue<string>("SmtpAddress"),
                 Configuration.GetValue<string>("ConfirmAddress"),
                 int.Parse(Configuration.GetValue<string>("SmtpPort")));
+
             var eventBusSettings = new EventBusSettings(Configuration.GetValue<string>("RabbitMqServerHostName"),
-                "/", "guest", "guest");
+                Configuration.GetValue<string>("RabbitMqServerVirtualHost"),
+                Configuration.GetValue<string>("RabbitMqAdminUserName"),
+                Configuration.GetValue<string>("RabbitMqAdminPassword"));
             var eventBus = new EventBus(eventBusSettings);
             eventBus.StartListening();
 
