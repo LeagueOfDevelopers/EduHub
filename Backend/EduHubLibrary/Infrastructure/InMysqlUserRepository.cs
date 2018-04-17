3be3f3b05739daedabbbd740cb71d0d62b33854a
yaroslav <bus.yaroslav@gmail.com>
2018-04-17 23:01:33 +0300

    user repo remarks

--- Backend/EduHubLibrary/Infrastructure/InMysqlUserRepository.cs
+++ Backend/EduHubLibrary/Infrastructure/InMysqlUserRepository.cs
@@ -4,7 +4,9 @@ using EduHubLibrary.Common;
 using EduHubLibrary.Data;
 using EduHubLibrary.Data.UserDtos;
 using EduHubLibrary.Domain;
+using EduHubLibrary.Domain.Exceptions;
 using EduHubLibrary.Extensions;
+using EnsureThat;
 using Microsoft.EntityFrameworkCore;
 
 namespace EduHubLibrary.Infrastructure
@@ -42,6 +44,9 @@ namespace EduHubLibrary.Infrastructure
                     .Include(u => u.Tags)
                     .Include(u => u.Notifies)
                     .FirstOrDefault(u => u.Id == user.Id);
+
+                Ensure.Any.IsNotNull(userDto, nameof(user),
+                    opt => opt.WithException(new UserNotFoundException(user.Id)));
 
                 _context.RemoveRange(userDto.Tags);
                 userDto.Tags.RemoveAll(t => true);
@@ -96,6 +101,8 @@ namespace EduHubLibrary.Infrastructure
                     .Include(u => u.Reviews)
                     .Include(u => u.Notifies)
                     .FirstOrDefault(u => u.Id == userId);
+                Ensure.Any.IsNotNull(userDto, nameof(userId),
+                    opt => opt.WithException(new UserNotFoundException(userId)));
                 var user = UserExtensions.ParseFromUserDto(userDto);
                 return user;
             }
@@ -112,6 +119,8 @@ namespace EduHubLibrary.Infrastructure
                     .Include(u => u.Reviews)
                     .Include(u => u.Notifies)
                     .FirstOrDefault(u => u.Email == email);
+                Ensure.Any.IsNotNull(userDto, nameof(email),
+                    opt=> opt.WithException(new UserNotFoundException(email)));
                 var user = UserExtensions.ParseFromUserDto(userDto);
                 _context.SaveChanges();
                 return user;
@@ -130,6 +139,9 @@ namespace EduHubLibrary.Infrastructure
                     .Include(u => u.Notifies)
                     .FirstOrDefault(u => u.Email == credentials.Email
                                          && u.PasswordHash == credentials.PasswordHash);
+                Ensure.Any.IsNotNull(userDto, nameof(credentials), 
+                    opt => opt.WithException(new UserNotFoundException()));
+
                 var user = UserExtensions.ParseFromUserDto(userDto);
                 _context.SaveChanges();
                 return user;
