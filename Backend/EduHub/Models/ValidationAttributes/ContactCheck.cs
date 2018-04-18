1455738a163d5fd39bc7f03ffb0a20c9a85d29e2
yaroslav <bus.yaroslav@gmail.com>
2018-04-19 01:29:28 +0300

    contact validation

--- Backend/EduHub/Models/ValidationAttributes/ContactCheck.cs
+++ Backend/EduHub/Models/ValidationAttributes/ContactCheck.cs
@@ -1,21 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace EduHub.Models.ValidationAttributes
 {
     public class ContactCheck : ValidationAttribute
     { 
-
         public override bool IsValid(object value)
         {
-            var list = value as IList<string>;
-            if (list != null)
+            const string pattern = @"^((http|https)://(vk.com|behance.net|twitter.com|ok.ru|facebook.com|instagram.com|github.com)/[0-9A-Za-z]+)$";
+            const string patternTumblr = @"^((https|http)://([0-9A-Za-z]+).tumblr.com)$";
+            const string mobilePattern = @"^(\d{11})$";
+            var multiPattern = $"({pattern})|({mobilePattern})|({patternTumblr})";
+            if (value is IList<string> list)
             {
-                list = list.ToList();
-                return true;
+                return list.ToList().All(s => Regex.IsMatch(s, multiPattern, RegexOptions.IgnoreCase));
             }
 
             return false;
