bdb07c64ff07e7b202c5b4de6b43c270e9946331
yaroslav <bus.yaroslav@gmail.com>
2018-06-09 09:24:09 +0300

    validation fix

--- Backend/EduHub/Models/ValidationAttributes/BirthCheck.cs
+++ Backend/EduHub/Models/ValidationAttributes/BirthCheck.cs
@@ -16,7 +16,7 @@ namespace EduHub.Models.ValidationAttributes
         {
             var birthDate = (DateTimeOffset) value;
             var current = DateTime.Now.Year;
-            return birthDate.Year >= _startYear && birthDate.Year <= current;
+            return (birthDate.Year >= _startYear && birthDate.Year <= current) || birthDate == DateTimeOffset.MinValue;
         }
     }
 }
